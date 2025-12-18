ed6eebe41f2e6aa8464a1f8a2b16b0533a2e97fe
AndreaBarbuto99 <barbutoandrea99@gmail.com>
2025-12-18 12:34:16 +0100

    js Snack for Methods 2

--- script.js
+++ script.js
@@ -1,39 +1,32 @@
-/* Crea un array composto da 10 automobili. 
-Ogni oggetto automobile avrà le seguenti proprietà: marca, modello e alimentazione 
-(benzina, diesel, gpl, elettrico, metano). Dividi le automobili in 3 array separati:
-nel primo array solo le auto a benzina, nel secondo solo le auto a diesel, 
-nel terzo il resto delle auto. Infine stampa separatamente i 3 array.*/
+/*A partire da un array di stringhe, 
+crea un secondo array formattando le stringhe del primo array in minuscolo 
+e con l’iniziale maiuscola. */
 
 
-const automobili = [
-    { marca: "Fiat", modello: "Panda", alimentazione: "benzina" },
-    { marca: "Volkswagen", modello: "Golf", alimentazione: "diesel" },
-    { marca: "Renault", modello: "Clio", alimentazione: "gpl" },
-    { marca: "Tesla", modello: "Model 3", alimentazione: "elettrico" },
-    { marca: "Opel", modello: "Corsa", alimentazione: "benzina" },
-    { marca: "Ford", modello: "Focus", alimentazione: "benzina" },
-    { marca: "Toyota", modello: "Yaris", alimentazione: "ibrido" },
-    { marca: "BMW", modello: "X1", alimentazione: "diesel" },
-    { marca: "Hyundai", modello: "Kona", alimentazione: "elettrico" },
-    { marca: "Lancia", modello: "Ypsilon", alimentazione: "gpl" }
+const parole = [
+  "CASA",        
+  "AlBum",       
+  "fiUme",       
+  "STELLA",      
+  "piano",       
+  "GaTtO",       
+  "LiBro",       
+  "MARE",        
+  "sole",        
+  "ViNo"         
 ];
 
-const benzinaAuto = automobili.filter ((auto)  =>
-{
-    return auto.alimentazione === "benzina";
-}
-);
-
-console.log(benzinaAuto);
-
-const dieselAuto = automobili.filter ((auto) => {
-    return auto.alimentazione === "diesel";
+const paroleFormat = parole.map ((parola) => {
+   min = parola.toLowerCase();
+   return min.charAt(0).toUpperCase() + min.slice(1);
 });
 
-console.log(dieselAuto);
+console.log(paroleFormat);
 
-const altreAuto = automobili.filter ((auto) => {
-    return auto.alimentazione !== "diesel" && auto.alimentazione !== "benzina";
-});
+// const capitalizedStrings = parole.map ((name) => {
+//     const firstLetter = name[0].toUpperCase();
+//     const otherLetter = name.substring(1).toLowerCase();
+//     return firstLetter + otherLetter;
+// })
 
-console.log(altreAuto);
+// console.log(capitalizedStrings)
